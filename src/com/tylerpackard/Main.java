12b70281eea36045dca3de5f2c43fb97c674cd60
TPackard <tyler.packard86@gmail.com>
2014-04-21 18:12:38 -0700

    Commented in JavaDocs
    In Main.java

--- src/com/tylerpackard/Main.java
+++ src/com/tylerpackard/Main.java
@@ -2,36 +2,52 @@ package com.tylerpackard;
 
 import com.tylerpackard.ui.Window;
 
+/**
+ * The class that starts and runs the program. It contains the Window, which controls its own subsystems.
+ */
 public class Main implements Runnable{
+	/**
+	 * The Window that contains the editor and subsystems
+	 */
 	private Window window;
 
 
+	/**
+	 * The method that starts the Main runnable and sets the Mac Menubar name.
+	 *
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Pixel Editor");
 		new Thread(new Main()).start();
 	}
 
+	/**
+	 * The run method of the Main runnable. It creates a new window starts the looped section of the code, pausing for
+	 * 10 milliseconds between each call of loop().
+	 *
+	 * @see #loop()
+	 */
 	@Override
 	public void run() {
-		// This all happens once when the program starts
 		window = new Window(720, 480);
 
 		while (true) {
 			loop();
-			sleep(10);
+
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
+	/**
+	 * Updates the window every 10 milliseconds
+	 */
 	void loop() {
 		window.update();
-	}
-
-	public static void sleep(long milliseconds) {
-		try {
-			Thread.sleep(milliseconds);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 	}
 }
