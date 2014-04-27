e9972005cc3fdbe50b9115e3f85dcbb20edc6b63
TPackard <tyler.packard86@gmail.com>
2014-04-27 00:30:26 -0700

    Commented in JavaDocs
    In ToolChooser.java

--- src/com/tylerpackard/toolbox/toolchooser/ToolChooser.java
+++ src/com/tylerpackard/toolbox/toolchooser/ToolChooser.java
@@ -9,17 +9,62 @@ import com.tylerpackard.ui.Window;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The ToolChooser contains all of the tools and creates an interface that lets the user select a tool.
+ *
+ * @author Tyler Packard
+ * @version 1
+ * @since 0.0.1
+ */
 public class ToolChooser extends JPanel implements Updatable {
+
+	/**
+	 * The Tool that is currently selected. Points to one of the more specific Tool constants.
+	 */
 	private Tool selectedTool;
+
+	/**
+	 * The Window that contains the ToolChooser.
+	 */
 	private Window parent;
+
+	/**
+	 * The ToolChooser's Pencil tool constant.
+	 */
 	private final Pencil PENCIL;
+
+	/**
+	 * The ToolChooser's Eraser tool constant.
+	 */
 	private final Eraser ERASER;
+
+	/**
+	 * The ToolChooser's Bucket tool constant.
+	 */
 	private final Bucket BUCKET;
+
+	/**
+	 * The ToolChooser's ColorPicker tool constant.
+	 */
 	private final ColorPicker PICKER;
+
+	/**
+	 * The width of the ToolChooser.
+	 */
 	private final int width;
+
+	/**
+	 * Whether or not to temporarily return the color picker.
+	 */
 	private boolean tempPicker = false;
 
 
+	/**
+	 * Creates a new ToolChooser and its Tools and sets its dimensions and position.
+	 *
+	 * @param parent The containing Window
+	 * @param colorChooser The ColorChooser to get colors from
+	 */
 	public ToolChooser(Window parent, ColorChooser colorChooser) {
 		this.parent = parent;
 		setBackground(new Color(0x444448));
@@ -40,25 +85,43 @@ public class ToolChooser extends JPanel implements Updatable {
 		reposition();
 	}
 
+	/**
+	 * Called by the parent Window when it needs to be updated, unused.
+	 */
 	@Override
 	public void update() {
 
 	}
 
+	/**
+	 * Repositions the ToolChooser inside the Window. Spans the entire height of the Window and stays on the right side.
+	 */
 	@Override
 	public void reposition() {
 		setBounds(parent.width() - width, 0, width, parent.height());
 	}
 
+	/**
+	 * Removes focus from any focusable objects. Unused.
+	 */
 	@Override
 	public void defocus() {
 
 	}
 
+	/**
+	 * @return The parent Window
+	 */
 	public Window getParent() {
 		return parent;
 	}
 
+	/**
+	 * Returns the Tool that the user last selected, or the color picker if the ToolChooser is told to temporarily
+	 * return the color picker.
+	 *
+	 * @return The selected Tool
+	 */
 	public Tool getSelectedTool() {
 		if (tempPicker) {
 			return PICKER;
@@ -67,16 +130,30 @@ public class ToolChooser extends JPanel implements Updatable {
 		}
 	}
 
+	/**
+	 * @return The containing Window's EditManager
+	 */
 	public EditManager getEditManager() {
 		return parent.getEditManager();
 	}
 
+	/**
+	 * Switches selection from the selected tool to the given one.
+	 *
+	 * @param tool The Tool to make selected
+	 */
 	public void requestSelection(Tool tool) {
 		selectedTool.deselect();
 		selectedTool = tool;
+		selectedTool.select();
 		parent.repaint();
 	}
 
+	/**
+	 * Draws the buttons for each of the tools.
+	 *
+	 * @param g The Graphics to paint with
+	 */
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -90,6 +167,11 @@ public class ToolChooser extends JPanel implements Updatable {
 		g.drawImage(PICKER.getIcon(), 8, 128, 32, 32, null);
 	}
 
+	/**
+	 * True if the ToolChooser should temporarily return a color picker instead of the selected tool, false otherwise.
+	 *
+	 * @param tempPicker Whether or not to use the temporary picker
+	 */
 	public void setTempPicker(boolean tempPicker) {
 		this.tempPicker = tempPicker;
 		parent.repaint();
