e9f156311e90cfc64fcb62872dd2f814366dfa50
TPackard <tyler.packard86@gmail.com>
2014-04-29 11:17:19 -0700

    Commented in JavaDocs
    In Updatable.java

--- src/com/tylerpackard/ui/Updatable.java
+++ src/com/tylerpackard/ui/Updatable.java
@@ -1,9 +1,22 @@
 package com.tylerpackard.ui;
 
+/**
+ * The Updatable contains useful methods for updatable GUI components.
+ */
 public interface Updatable {
+
+	/**
+	 * Update should contain code that does anything that the component needs to do periodically, such as repainting.
+	 */
 	void update();
 
+	/**
+	 * Reposition should contain code that repositions the component inside of its parent.
+	 */
 	void reposition();
 
+	/**
+	 * Defocus should strip focus from all child components that have focus.
+	 */
 	void defocus();
 }
